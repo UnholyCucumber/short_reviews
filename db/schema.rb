ddff10f5a3c5e8a93650d12677a2d1a9adb79177
Andy Zhang <andyzhang@live.ca>
2018-09-10 22:39:00 -0400

    Fixed film

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902190530) do
+ActiveRecord::Schema.define(version: 20180911021534) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180902190530) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.string "Language"
   end
 
   create_table "games", force: :cascade do |t|
